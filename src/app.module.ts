fb1dda83263e077ee70652e1b389e95835a4ca69
william-esteban <114128992+william-esteban@users.noreply.github.com>
2024-05-31 11:34:35 -0500

    connection postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,12 +8,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.POSTGRES_HOST,
-      port: +process.env.DB_PORT,
+      port: +process.env.DB_PORT || 5432,
       username: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DATABASE,
       autoLoadEntities: true,
       synchronize: true,
+      ssl: true,
     }),
 
   ],
